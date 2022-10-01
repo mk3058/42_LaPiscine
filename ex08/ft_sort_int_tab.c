5c426cb1218bf879a331d690fbdd8039a901f2df
Minkyu Kim <minkyuki@c2r5s2.42seoul.kr>
2022-10-02 07:38:32 +0900

    norm 안맞춘거 수정

--- ex08/ft_sort_int_tab.c
+++ ex08/ft_sort_int_tab.c
@@ -6,15 +6,15 @@
 /*   By: minkyuki <minkyuki@student.42seoul.>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/01 22:16:23 by minkyuki          #+#    #+#             */
-/*   Updated: 2022/10/01 22:20:18 by minkyuki         ###   ########.fr       */
+/*   Updated: 2022/10/02 07:37:49 by minkyuki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 void	ft_sort_int_tab(int *tab, int size)
 {
-	int i;
-	int j;
-	int tmp;
+	int	i;
+	int	j;
+	int	tmp;
 
 	i = 0;
 	j = 0;
@@ -23,7 +23,7 @@ void	ft_sort_int_tab(int *tab, int size)
 		j = 0;
 		while (j < size - 1 - i)
 		{
-			if(tab[j] > tab[j + 1])
+			if (tab[j] > tab[j + 1])
 			{
 				tmp = tab[j];
 				tab[j] = tab[j + 1];
